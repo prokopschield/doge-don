791b09d5bd5a0eac40f2dc5d1fd82fe967565871
Prokop <prokop.schield@protonmail.com>
2024-01-04 20:20:29 +0100

    feat: v0.1.0-6: add fmt_rc()

--- src/formatter.ts
+++ src/formatter.ts
@@ -32,3 +32,31 @@ export function* formatter<T>(arg: T, indenter = "\t", newline = "\n") {
 export function fmt<T>(arg: T, indenter = "\t", newline = "\n") {
 	return [...formatter(arg, indenter, newline)].join("");
 }
+
+export function* formatter_rc<T>(arg: T) {
+	let depth = 0;
+
+	for (const token of tokenizer(
+		typeof arg === "string" ? decode(arg) : arg
+	)) {
+		if (token === symbols["["]) {
+			if (depth++) {
+				yield "[";
+			}
+		} else if (token === symbols["]"]) {
+			if (--depth) {
+				yield "]";
+			}
+		} else if (token === symbols[","]) {
+			yield depth > 1 ? "," : "\n";
+		} else if (token === symbols["=>"]) {
+			yield depth > 1 ? "=>" : "=";
+		} else {
+			yield print_token(token);
+		}
+	}
+}
+
+export function fmt_rc<T>(arg: T) {
+	return [...formatter_rc(arg)].join("");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { decode } from "./decode";
 export { encode } from "./encode";
-export { formatter, fmt } from "./formatter";
+export * from "./formatter";
 export { lexer } from "./lexer";
 export { parser } from "./parser";
 export { read } from "./read";
