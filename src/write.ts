8f9dc8f4b068bd790f2a7fe612b7d1e08fb67e55
Prokop <prokop.schield@protonmail.com>
2024-01-11 16:08:32 +0100

    Release: v0.1.0-7

--- src/write.ts
+++ src/write.ts
@@ -1,16 +1,18 @@
 import fs from "fs";
 
-import { fmt } from "./formatter";
+import { fmt, fmt_rc } from "./formatter";
 
-export function write<T>(filename: string, arg: T) {
+export function write<T>(filename: string, arg: T, rc = false) {
+	const formatter = rc ? fmt_rc : fmt;
+
 	try {
 		if (
 			typeof window === "undefined" ||
 			typeof localStorage === "undefined"
 		) {
-			fs.writeFileSync(filename, fmt(arg, "\t", "\n") + "\n");
+			fs.writeFileSync(filename, formatter(arg, "\t", "\n") + "\n");
 		} else {
-			localStorage.setItem(filename, fmt(arg, "", " "));
+			localStorage.setItem(filename, formatter(arg, "", " "));
 		}
 	} catch {
 		return "";
